0495d79806009c5a9f034e14737a87d347504fc0
shourok <78165565+shourok@users.noreply.github.com>
2026-01-14 14:50:56 +0200

    Update onos_client.py

--- onos/onos_client.py
+++ onos/onos_client.py
@@ -1,12 +1,15 @@
 import requests
 from requests.auth import HTTPBasicAuth
+
 class ONOSClient:
-    def __init__(self, url, user, pwd):
-        self.url = url
+    def __init__(self, url: str, user: str, pwd: str):
+        self.url = url.rstrip("/")
         self.auth = HTTPBasicAuth(user, pwd)
-    def deploy_intent(self, intent):
+
+    def post_intent(self, intent: dict):
         return requests.post(
             f"{self.url}/intents",
             json=intent,
-            auth=self.auth
+            auth=self.auth,
+            timeout=10
         )
